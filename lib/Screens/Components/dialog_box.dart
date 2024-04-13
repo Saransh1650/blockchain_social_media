b2ff89baafdc111e4440482163b2ea59d4b2d695
Saransh1650 <singhalsaransh40@gmail.com>
2024-04-13 12:01:29 +0530

    adding features

--- lib/Screens/Components/dialog_box.dart
+++ lib/Screens/Components/dialog_box.dart
@@ -1,82 +1,113 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:animated_icon/animated_icon.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:blockchain_dapp/State%20Management/web3service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animated_button/flutter_animated_button.dart';
 import 'package:flutter_ipfs/flutter_ipfs.dart';
+import 'package:get/get.dart';
 
 class DialogBox extends StatelessWidget {
   const DialogBox({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    final Web3Service w3c = Get.put(Web3Service());
+    final myController = TextEditingController();
+
     return Container(
       padding: const EdgeInsets.all(10),
       child: Column(
         children: [
-          const TextField(
-            style: TextStyle(color: Colors.white),
+          AnimatedTextKit(totalRepeatCount: 1, animatedTexts: [
+            TyperAnimatedText("Add Image or Video",
+                textStyle: TextStyle(color: Colors.white, fontSize: 25))
+          ]),
+          SizedBox(
+            height: 5,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(color: Colors.blueAccent)),
+                child: AnimateIcon(
+                  onTap: () async {
+                    await ImagePickerService.pickImage(context);
+                    print(ImagePickerService.cid);
+                  },
+                  animateIcon: AnimateIcons.file,
+                  iconType: IconType.continueAnimation,
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(color: Colors.blueAccent)),
+                  child: AnimateIcon(
+                    onTap: () => VideoPickerService.pickVideo(context),
+                    animateIcon: AnimateIcons.liveVideo,
+                    iconType: IconType.continueAnimation,
+                    color: Colors.white,
+                  )),
+            ],
+          ),
+          SizedBox(height: 10),
+           TextField(
+            
+            controller: myController,
 
+            style: TextStyle(color: Colors.white),
             decoration: InputDecoration(
-                
                 hintText: "Write your thoughts",
                 hintStyle: TextStyle(color: Colors.white)),
           ),
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.image,
-                color: Colors.white,
-              )),
-              Center(
-            child: ElevatedButton(
-              onPressed: () async {
-                await ImagePickerService.pickImage(context);
-                print(ImagePickerService.cid);
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.black,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25),
-                ),
-              ),
-              child: const SizedBox(
-                height: 50,
-                child: Center(
-                  child: Text(
-                    'Upload Image',
-                    style: TextStyle(fontSize: 18, fontFamily: 'Brand-Bold'),
-                  ),
-                ),
-              ),
-            ),
-          ),
           const SizedBox(height: 20),
-          Center(
-            child: ElevatedButton(
-              onPressed: () => VideoPickerService.pickVideo(context),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.black,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25),
-                ),
-              ),
-              child: const SizedBox(
-                height: 50,
-                child: Center(
-                  child: Text(
-                    'Upload Video',
-                    style: TextStyle(fontSize: 18, fontFamily: 'Brand-Bold'),
-                  ),
-                ),
-              ),
-            ),
+          AnimatedButton(
+           
+            isReverse: true,
+            animatedOn: AnimatedOn.onTap,
+            borderColor: Colors.blueAccent,
+            backgroundColor: Colors.white,
+            selectedBackgroundColor: Colors.black,
+            borderRadius: 10,
+            width: 200,
+            text: 'Post',
+            selectedTextColor: Colors.blueAccent,
+            transitionType: TransitionType.LEFT_TO_RIGHT,
+            textStyle: TextStyle(
+                fontSize: 28,
+                letterSpacing: 5,
+                color: Colors.blueAccent,
+                fontWeight: FontWeight.w300),
+            onPress: () async {
+             try{ if(ImagePickerService.cid == ""){
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text("Please select an image"),
+                ));
+                
+              }
+              else{
+                await w3c.callWriteFunction(myController.text,ImagePickerService.cid);
+              }
+              }
+              catch(e){
+                print(e);
+              }
+            },
           ),
-          FloatingActionButton(
-            backgroundColor: Colors.black,
-            onPressed: () {},
-            child: const Text(
-              "Post ->",
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-
+          SizedBox(
+            height: 10,
+          )
         ],
       ),
     );
--- lib/State Management/web3service.dart
+++ lib/State Management/web3service.dart
@@ -1,11 +1,9 @@
 import 'dart:convert';
-
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:web3modal_flutter/web3modal_flutter.dart';
 
 class Web3Service extends GetxController {
-
   @override
   Future<void> onInit() async {
     super.onInit();
@@ -14,18 +12,18 @@ class Web3Service extends GetxController {
   }
 
   final w3mService = W3MService(
-  projectId: 'a9a9881b4a61d564555fe9e76cd0980d',
-  metadata: const PairingMetadata(
-    name: 'EtherFrame',
-    description: 'Decentralized Photo App',
-    url: 'https://www.walletconnect.com/',
-    icons: ['https://walletconnect.com/walletconnect-logo.png'],
-    redirect: Redirect(
-      native: 'flutterdapp://',
-      universal: 'https://www.walletconnect.com',
+    projectId: 'a9a9881b4a61d564555fe9e76cd0980d',
+    metadata: const PairingMetadata(
+      name: 'EtherFrame',
+      description: 'Decentralized Photo App',
+      url: 'https://www.walletconnect.com/',
+      icons: ['https://walletconnect.com/walletconnect-logo.png'],
+      redirect: Redirect(
+        native: 'flutterdapp://',
+        universal: 'https://www.walletconnect.com',
+      ),
     ),
-  ),
-);
+  );
 
 // Register callbacks on the Web3App you'd like to use. See `Events` section.
   Future<void> callReadFunction() async {
@@ -63,7 +61,7 @@ class Web3Service extends GetxController {
     }
   }
 
-  Future<void> callWriteFunction() async {
+  Future<void> callWriteFunction(String title, String ipfs) async {
     String abiJsonString = await rootBundle.loadString('assets/abi.json');
     List<dynamic> abiJson = jsonDecode(abiJsonString);
     // Create DeployedContract object using contract's ABI and address
@@ -76,32 +74,22 @@ class Web3Service extends GetxController {
         EthereumAddress.fromHex('0xD07B2318a33579Af3057D151666508E480e83077'),
       );
 
-      // Get balance of wallet
-      // var result = await w3mService.requestReadContract(
-      //     deployedContract: deployedContract,
-      //     functionName: 'getComments',
-      //     rpcUrl: 'https://polygon-testnet.public.blastapi.io',
-      //     parameters: [BigInt.from(0)]);
-      // print(result);
-
-      await w3mService.launchConnectedWallet();
+await w3mService.launchConnectedWallet();
       await w3mService.requestWriteContract(
+        method: "POST",
           topic: w3mService.session!.topic.toString(),
-          chainId: 'eip155:11155111',
-          rpcUrl: 'https://ethereum-sepolia-rpc.publicnode.com',
+          chainId: "eip155:80001",
+          rpcUrl: 'https://polygon-mumbai.blockpi.network/v1/rpc/public',
           deployedContract: deployedContract,
-          functionName: 'ViewAllPosts',
-          parameters: [],
+          functionName: 'postMedia',
+          parameters: [title, ipfs],
           transaction: Transaction(
             from: EthereumAddress.fromHex(
                 '0xAd69146A55DcA5E6a7FbA9315683C9226E2f6c33'),
           ));
+      
     } catch (e) {
       print(e);
     }
   }
-
-  
-
-
 }
